d7f1cfb60fd87088fbab671695cac156124faaa2
mazemaze <yujiro2345@gmail.com>
2021-06-24 21:12:35 +0900

    Cleaned code

---
+++ salon_creator/lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SalonCreatorApp extends StatelessWidget {
+  const SalonCreatorApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Gospel Crowd"),
+        ),
+      ),
+    );
+  }
+}
