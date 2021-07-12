abfe987f776f3c2c71bade9ce6e0e86343eb746c
Samuel Bangari <samuel.bangari@jp.kpmg.com>
2021-07-12 22:26:35 +0900

    Added top level drawer menu for Salon creator

--- salon_creator/lib/app.dart
+++ salon_creator/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:salon_creator/common/color.dart';
 import 'package:salon_creator/constant/constants.dart' as constants;
 import 'package:salon_creator/screens/home.dart';
 import 'package:salon_creator/screens/login_page.dart';
@@ -11,6 +12,7 @@ class SalonCreatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: _buildThemeData(),
       routes: {
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
@@ -19,7 +21,7 @@ class SalonCreatorApp extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
           if (snapshot.hasData) {
-            return _buildHomePage(snapshot);
+            return _buildHomePage();
           }
           return LoginPage();
         },
@@ -27,11 +29,36 @@ class SalonCreatorApp extends StatelessWidget {
     );
   }
 
-  Widget _buildHomePage(AsyncSnapshot<User> snapshot) {
+  ThemeData _buildThemeData() {
+    return ThemeData(
+      fontFamily: 'NotoSansJP',
+      primaryColor: primaryColor,
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(foregroundColor),
+        ),
+      ),
+      dividerColor: Color.fromRGBO(193, 193, 193, 1),
+      appBarTheme: AppBarTheme(
+        shadowColor: Colors.transparent,
+        color: Colors.white,
+        foregroundColor: Colors.black,
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontFamily: 'NotoSansJP',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHomePage() {
     return StreamBuilder<DocumentSnapshot>(
       stream: FirebaseFirestore.instance
           .collection(constants.DBCollection.users)
-          .doc(snapshot.data.email)
+          .doc(FirebaseAuth.instance.currentUser.email)
           .snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
